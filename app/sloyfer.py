e1aa90d87aa4dbf11e107cf29d3ef9808286b5f6
Kristoffer1212 <60184468+Kristoffer1212@users.noreply.github.com>
2020-03-17 14:33:18 +0100

    live graf

--- app/sloyfer.py
+++ app/sloyfer.py
@@ -7,9 +7,37 @@ import plotly
 import random
 import plotly.graph_objs as go
 from collections import deque
+from cosmosDB import read_from_db
+
 
 
 #app = dash.Dash(__name__)
+
+#temp data
+query = "SELECT * FROM heatTrace1 WHERE (heatTrace1.deviceType = 'tempSensor' AND heatTrace1.deviceEui = '70-b3-d5-80-a0-10-94-46') ORDER BY heatTrace1.timeReceived DESC"
+container_name = "heatTrace1"
+items = read_from_db(container_name, query)
+temp_ht1=[]
+for i in items:
+    temp_ht1.append(i['temperature'])
+ts_ht1=[]
+for i in items:
+    ts_ht1.append(i['_ts'])
+#temp_ht1.reverse()
+#ts_ht1.reverse()
+print(temp_ht1)
+
+
+#Powerwitch data
+query = "SELECT * FROM heatTrace1 WHERE heatTrace1.deviceType = 'powerSwitch' ORDER BY heatTrace1.timeReceived DESC"
+container_name = "heatTrace1"
+items = read_from_db(container_name, query)
+actEnergy_ht1 = items[0]['activeEnergy']
+rctEnergy_ht1 = items[0]['reactiveEnergy']
+appEnergy_ht1 = items[0]['apparentEnergy']
+volt_ht1 = items[0]['voltage']
+freq_ht1 = items[0]['frequency']
+
 
 layout = html.Div([
     html.Label('Regulerings-sløyfe'),
@@ -42,26 +70,32 @@ layout = html.Div([
 )
 
 def callbacks(app):
-    X = deque(maxlen=20)
-    X.append(1)
-    Y = deque(maxlen=20)
-    Y.append(1)
-
+    
     @app.callback(Output('live-graph', 'figure'),
-            [Input('graph-update', 'n_intervals')])
+            [Input('graph-update', 'n_intervals'),
+            ])
+            
     def update_graph_scatter(n):
-        X.append(X[-1]+1)
-        Y.append(Y[-1]+Y[-1]*random.uniform(-0.1,0.1))
+        try:
+            X=ts_ht1[:-20]
+            Y=temp_ht1[:-20]
 
-        data = plotly.graph_objs.Scatter(
-                x=list(X),
-                y=list(Y),
+            data = plotly.graph_objs.Scatter(
+                y=Y,
+                x=X,
                 name='Scatter',
                 mode= 'lines+markers'
                 )
 
-        return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+            return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
                                                     yaxis=dict(range=[min(Y),max(Y)]),)}
+      
+     
+        except Exception as e:
+            with open('errors.txt','a') as f:
+                f.write(str(e))
+                f.write('\n')                                               
+
 
 
 #if __name__ == '__main__':
