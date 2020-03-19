9591e3079db7c8a04adc4c3c5dc9a9dff1a3b556
Dawid Kalicki <dawod28@gmail.com>
2020-03-19 18:14:00 +0100

    table_not_working

--- app/dashApps/alarmer.py
+++ app/dashApps/alarmer.py
@@ -13,34 +13,32 @@ from cosmosDB import read_from_db
 from dashApps.layout import header
 from dashApps.layout import callbacks as layout_callbacks
 
-def get_alarms():
+def get_alarm_table():
     #get data where temp is <10 or >30
     query = "SELECT * FROM {0} WHERE {0}.deviceType = 'tempSensor' AND ({0}.temperature < 10 OR {0}.temperature > 30 ) ORDER BY {0}.timeReceived DESC"
     abnormal_values = read_from_db('heatTrace1', query)
-    return abnormal_values
 
-
-
-table_header = [html.Thead(html.Tr([html.Td("Time"), html.Td("Device placement"), html.Td("Device Eui"), html.Td("Value"),]))]
-table_rows = []
-
-for item in get_alarms():
-    time = item["timeReceived"]
-    devicePlacement = item["devicePlacement"]
-    deviceEui = item["deviceEui"]
-    temperature = item["temperature"]
-    row = html.Tr([html.Td(time), html.Td(devicePlacement), html.Td(deviceEui), html.Td(temperature)])
-    table_rows.append(row)
+    table_header = [html.Thead(html.Tr([html.Th("Time"), html.Th("Device placement"), html.Th("Device Eui"), html.Th("Value"),]))]
+    table_rows = []
     
-table_body=[html.Tbody(table_rows)]
-
-alarm_table = dbc.Table(table_header+table_body, bordered=True)
-
+    for item in abnormal_values:
+        time = item["timeReceived"]
+        devicePlacement = item["devicePlacement"]
+        deviceEui = item["deviceEui"]
+        temperature = item["temperature"]
+        row = html.Tr([html.Td(time), html.Td(devicePlacement), html.Td(deviceEui), html.Td("{} °C".format(temperature))])
+        table_rows.append(row)
+        
+    table_body=[html.Tbody(table_rows)]
+    
+    return dbc.Table(table_header+table_body, bordered=True)
+    
 
 layout = html.Div([
+    dcc.Interval(id='refresh', n_intervals=0, interval=10*1000),
     header,
     dbc.Container(
-        alarm_table
+        html.Div(id='table', className='tableFixHead')
     ),# Container
     ])# Div
 
@@ -48,6 +46,12 @@ layout = html.Div([
 def callbacks(app):
     layout_callbacks(app)
 
+    @app.callback(
+        Output(component_id='table', component_property='children'),
+        [Input(component_id='refresh', component_property='n_intervals')])
+    def display_layout(n):
+        #print(n)
+        return [get_alarm_table()]
 
         
 #if __name__ == "__main__":
