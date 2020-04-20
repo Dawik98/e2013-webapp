eb1cd7714a42cefae0d9ab68efc7666c2c018c30
Dawid Kalicki <dawod28@gmail.com>
2020-04-20 11:42:40 +0200

    merge fix

--- app/dashApps/home.py
+++ app/dashApps/home.py
@@ -65,12 +65,11 @@ layout = html.Div([
             min=0,
         )
     
-    ]),
-
-    ])
+    ]), ])
 
 
 def callbacks(app):
+    layout_callbacks(app)
     
     @app.callback([Output('DB-indicator', 'label'),
                     Output('DB-indicator', 'value'),
