a1a7488acd7a6e5ab3bc9c5198056b37add251bb
Kristoffer1212 <60184468+Kristoffer1212@users.noreply.github.com>
2020-04-22 15:01:46 +0200

    Knapp som styrer måleområdet på¨trend

--- app/dashApps/sloyfer.py
+++ app/dashApps/sloyfer.py
@@ -67,17 +67,21 @@ dbc.Container([
     #Site title genereres av funksjon
     dbc.Row([html.Div(id='site-title-div')]),
     dbc.Row([
-        dbc.Col([
-        html.H5('Fra dato'),
-        dbc.Input(id='fra_Dato', value=fra_dato.strftime("%Y-%m-%d %H:%M:%S"), type='text',placeholder="YYYY-MM-DD HH:MM:SS",debounce=True)
+        dbc.Col([ 
+            html.H5('Måleområdet:'),
+        ],width=2),
+        dbc.Col([   
+            dbc.DropdownMenu(label = 'Siste 6 timer', id='dropdown-område', children=[
+            dbc.DropdownMenuItem('Siste time', id='1hours'),
+            dbc.DropdownMenuItem('Siste 6 timer', id='6hours'),
+            dbc.DropdownMenuItem('Siste 12 timer', id='12hours'),
+            dbc.DropdownMenuItem('Siste dag', id='day'),
+            dbc.DropdownMenuItem('Siste uke', id='week'),
+            ])
         ], width=2),
-
-        dbc.Col([
-        html.H5('Til dato'),
-        dbc.Input(id='til_Dato', value='', type='text',placeholder="<Empty> for live",debounce=True)
-        ], width=2),
-    ])
-    ]),
+        ]),      
+]),
+        
 dbc.Container([
     dbc.Row([
         dbc.Col([
@@ -131,8 +135,7 @@ def callbacks(app):
     layout_callbacks(app)
     update_sløyfe_callback(app, [['site-title-div', get_site_title]])
     #Laster inn ny data i datofeltet som kjøres når siden lastes inn
-    @app.callback([Output('fra_Dato', 'value'),
-                    Output('måle-valg', 'options'),
+    @app.callback([ Output('måle-valg', 'options'),
                     Output('Overskrift-Graf', 'children')],
                     [Input('refresh-dato', 'n_clicks')],
                     [State(component_id='url', component_property='pathname'),
@@ -144,12 +147,6 @@ def callbacks(app):
         states = ctx.states
         pathname = states['url.pathname']
         sløyfe_valg = get_sløyfe_from_pathname(pathname)
-
-        #Finner nå tid som settes inn i datofelt
-        til_dato_UTC = datetime.now()
-        til_dato = til_dato_UTC.astimezone(pytz.timezone('Europe/Oslo'))
-        fra_dato= til_dato + relativedelta(hours=-6)
-        fra_dato=fra_dato.strftime("%Y-%m-%d %H:%M:%S")
 
         #Tømmer målevalg for rele dersom sløyfen ikke har et rele.
         settings=get_settings()
@@ -165,49 +162,62 @@ def callbacks(app):
         else:
             options=[{'label': s,'value': s} for s in målinger_dict.keys()] 
             overskrift= "Måle valg, relé"
-        return fra_dato, options, overskrift
+        return options, overskrift
 
     #Live temperatur data
     @app.callback(Output('live-graph', 'figure'),
             [Input('graph-update', 'n_intervals'),
-            Input('fra_Dato', 'value'),
-            Input('til_Dato','value')],
+            Input('dropdown-område','label')],
             [State(component_id='url', component_property='pathname'),
             ])
     
 
-    def update_graph_scatter(n,fra_dato, til_dato, url):
+    def update_graph_scatter(n,dropdown_område, url):
         try:  
             #Finner sløyfevalg
             ctx = dash.callback_context
             states = ctx.states
             pathname = states['url.pathname']
             sløyfe_valg = get_sløyfe_from_pathname(pathname)
+            #Gjør ænsket måleområde om til tall
+            if dropdown_område == "Siste time":
+                offset=1
+            elif dropdown_område == "Siste 6 timer": 
+                offset=6
+            elif dropdown_område == "Siste 12 timer": 
+                offset=12
+            elif dropdown_område == "Siste dag": 
+                offset=24
+            elif dropdown_område == "Siste uke": 
+                offset=168
 
-            #Dersom datofletet er tomt returnerer vi tom graf.
-            #Hindrer at siden fryser
-            if fra_dato == "":
-                 return {'data': [], 'layout': {}}
+            #finner området som skal hentes fra databse
+            til_dato_UTC = datetime.now()
+            til_dato = til_dato_UTC.astimezone(pytz.timezone('Europe/Oslo'))
+            fra_dato= til_dato + relativedelta(hours=-offset)
+            fra_dato=fra_dato.strftime("%Y-%m-%d %H:%M:%S")
+
+            #henter inn ny data fra database
+            ts_UTC, temp = update_tempData(sløyfe_valg, fra_dato, til_dato)
+
+            #Dersom måle array er tom returneres tom graf, hindrer at siden fryser
+            if not temp:
+                return {'data': [], 'layout': {}}
             else:
-                #henter inn ny data fra database
-                ts_UTC, temp = update_tempData(sløyfe_valg, fra_dato, til_dato)
-                if not temp:
-                    return {'data': [], 'layout': {}}
-                else:
-                    #tilordner X og Y på graf
-                    X=ts_UTC
-                    Y=temp
-                    data = plotly.graph_objs.Scatter(
-                            y=Y,
-                            x=X,
-                            name='Scatter',
-                            mode= 'lines+markers'
-                            )
-                    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[(min(X)),(max(X))]),
-                                                                yaxis=dict(range=[0,120],title='Temperatur [°C]'),
-                                                                title='Temperaturmåling',
-                                                                #margin={'l':300,'r':100,'t':5,'b':50},
-                                                            )}
+                #tilordner X og Y på graf
+                X=ts_UTC
+                Y=temp
+                data = plotly.graph_objs.Scatter(
+                        y=Y,
+                        x=X,
+                        name='Scatter',
+                        mode= 'lines+markers'
+                        )
+                return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[(min(X)),(max(X))]),
+                                                            yaxis=dict(range=[0,120],title='Temperatur [°C]'),
+                                                            title='Temperaturmåling',
+                                                            #margin={'l':300,'r':100,'t':5,'b':50},
+                                                        )}
         #Ved feilmelding skrives det til error txt fil.                                                 
         except Exception as e:
             with open('errors.txt','a') as f:
@@ -216,45 +226,84 @@ def callbacks(app):
     #Live målerelé graf
     @app.callback(Output('live-graph2', 'figure'),
                 [Input('graph-update2', 'n_intervals'),
-                Input('fra_Dato', 'value'),
-                Input('til_Dato','value'),
+                Input('dropdown-område', 'label'),
                 Input('måle-valg', 'value')],
                 [State(component_id='url', component_property='pathname'),
                 ]) 
     #Funksjon som returnerer data som skal plottes
-    def update_graph_scatter2(n,fra_dato, til_dato, måle_valg, url):
+    def update_graph_scatter2(n,dropdown_område, måle_valg, url):
         try:
             ctx = dash.callback_context
             states = ctx.states
             pathname = states['url.pathname']
             sløyfe_valg = get_sløyfe_from_pathname(pathname)
-            #Dersom datofletet er tomt returnerer vi tom graf.
+
+            #Gjør ønsket måleområde om til tall
+            if dropdown_område == "Siste time":
+                offset=1
+            elif dropdown_område == "Siste 6 timer": 
+                offset=6
+            elif dropdown_område == "Siste 12 timer": 
+                offset=12
+            elif dropdown_område == "Siste dag": 
+                offset=24
+            elif dropdown_område == "Siste uke": 
+                offset=168
+           
+            
+            #finner området som skal hentes fra databse
+            til_dato_UTC = datetime.now()
+            til_dato = til_dato_UTC.astimezone(pytz.timezone('Europe/Oslo'))
+            fra_dato= til_dato + relativedelta(hours=-offset)
+            fra_dato=fra_dato.strftime("%Y-%m-%d %H:%M:%S")
+            print(dropdown_område)
+
+            #Laster inn ny data fra database
+            meterData = update_meterData(sløyfe_valg,fra_dato, til_dato)
+            #Tilorder data til x og y etter målevalg
+            X=meterData["timeReceived"]
+            Y=meterData[målinger_dict[måle_valg]]
+            print(Y)
+
             #Hindrer at siden fryser
-            if fra_dato == "":
+            if not X:
                 return {'data': [], 'layout': {}}
-            else: 
-                #Laster inn ny data fra database
-                meterData = update_meterData(sløyfe_valg,fra_dato, til_dato)
-                #Tilorder data til x og y etter målevalg
-                X=meterData["timeReceived"]
-                Y=meterData[målinger_dict[måle_valg]]
-                if not X:
-                    return {'data': [], 'layout': {}}
-                else:
-                    #Returnerer graf med data og layout
-                    data = plotly.graph_objs.Scatter(
-                            y=Y,
-                            x=X,
-                            name='Scatter',
-                            mode= 'lines+markers'
-                            )  
-                    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[(min(X)),(max(X))]),
-                                                                yaxis=dict(range=[(min(Y)*.95),(max(Y)*1.05)],
-                                                                            title=enhet_dict[måle_valg], tickangle=0,),
-                                                                title='Målerelé: {}'.format(måle_valg),
-                                                                #margin={'l':100,'r':100,'t':50,'b':50},
-                                                                )}                                                                                                                                                            
+            else:
+                #Returnerer graf med data og layout
+                data = plotly.graph_objs.Scatter(
+                        y=Y,
+                        x=X,
+                        name='Scatter',
+                        mode= 'lines+markers'
+                        )  
+                return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[(min(X)),(max(X))]),
+                                                            yaxis=dict(range=[(min(Y)*.95),(max(Y)*1.05)],
+                                                                        title=enhet_dict[måle_valg], tickangle=0,),
+                                                            title='Målerelé: {}'.format(måle_valg),
+                                                            #margin={'l':100,'r':100,'t':50,'b':50},
+                                                            )}                                                                                                                                                            
         except Exception as e:
             with open('errors.txt','a') as f:
                 f.write(str(e))
                 f.write('\n')
+    @app.callback(
+            Output(component_id='dropdown-område', component_property='label'),
+            [Input(component_id='1hours', component_property='n_clicks'),
+            Input(component_id='6hours', component_property='n_clicks'),
+            Input(component_id='12hours', component_property='n_clicks'),
+            Input(component_id='day', component_property='n_clicks'),
+            Input(component_id='week', component_property='n_clicks'),
+            ])
+    def update_label(n_1hours, n_6hours, n_12hours, n_day, n_week):
+        id_lookup = {'1hours':'Siste time', '6hours':'Siste 6 timer', '12hours':'Siste 12 timer', 'day':'Siste dag', 'week': 'Siste uke'}
+
+        ctx = dash.callback_context
+
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+        label_clicked = id_lookup[button_id]
+
+
+        if (n_1hours is None and n_6hours is None and n_12hours is None and n_day is None and n_week is None) or not ctx.triggered:
+            return 'Siste 6 timer'
+
+        return label_clicked
