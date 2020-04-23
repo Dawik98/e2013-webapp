f24f44da8f39e9aef526b24454218c98c1fee452
Kristoffer1212 <60184468+Kristoffer1212@users.noreply.github.com>
2020-04-23 11:41:43 +0200

    Trend med knapp for målerele, og mer intuitivt dersom det ikke mer noen målepunkter i valgt periode

--- app/dashApps/sloyfer.py
+++ app/dashApps/sloyfer.py
@@ -67,11 +67,9 @@ dbc.Container([
     #Site title genereres av funksjon
     dbc.Row([html.Div(id='site-title-div')]),
     dbc.Row([
-        dbc.Col([ 
-            html.H5('Måleområdet:'),
-        ],width=2),
         dbc.Col([   
-            dbc.DropdownMenu(label = 'Siste 6 timer', id='dropdown-område', children=[
+            html.H5('Periode'),
+            dbc.DropdownMenu(label = 'Siste 6 timer', id='dropdown-område1', children=[
             dbc.DropdownMenuItem('Siste time', id='1hours'),
             dbc.DropdownMenuItem('Siste 6 timer', id='6hours'),
             dbc.DropdownMenuItem('Siste 12 timer', id='12hours'),
@@ -99,14 +97,24 @@ dbc.Container([
 dbc.Container([
     dbc.Row([
         dbc.Col([
+            html.H5('Periode'),   
+            dbc.DropdownMenu(label = 'Siste 6 timer', id='dropdown-område2', children=[
+            dbc.DropdownMenuItem('Siste time', id='1hours_rele'),
+            dbc.DropdownMenuItem('Siste 6 timer', id='6hours_rele'),
+            dbc.DropdownMenuItem('Siste 12 timer', id='12hours_rele'),
+            dbc.DropdownMenuItem('Siste dag', id='day_rele'),
+            dbc.DropdownMenuItem('Siste uke', id='week_rele'),
+            ])
+        ], width=2),
+        dbc.Col([
             html.H5((html.Div(id='Overskrift-Graf'))),
             dcc.Dropdown(
                     id='måle-valg',
                     options=[{'label': s,'value': s} for s in målinger_dict.keys()],
                     value='Aktiv effekt',
                 ),
-            ], width=5)
-    ])
+            ], width=5),
+    ]),
 ]),
 #Graf for målerele
 dbc.Container([
@@ -155,7 +163,6 @@ def callbacks(app):
         for i in range(0, nb_items):
             devices.append(settings[sløyfe_valg]['devices'][i]['deviceType'])
         if 'Power Switch' not in devices:
-            print("Ingen powerswitch")
             #Returnerer tom dropdown meny
             options={'label': "",'value': ""}
             overskrift = "Ingen målerele på denne sløyfen!"
@@ -167,7 +174,7 @@ def callbacks(app):
     #Live temperatur data
     @app.callback(Output('live-graph', 'figure'),
             [Input('graph-update', 'n_intervals'),
-            Input('dropdown-område','label')],
+            Input('dropdown-område1','label')],
             [State(component_id='url', component_property='pathname'),
             ])
     
@@ -202,7 +209,11 @@ def callbacks(app):
 
             #Dersom måle array er tom returneres tom graf, hindrer at siden fryser
             if not temp:
-                return {'data': [], 'layout': {}}
+                return {'data': [], 'layout' : go.Layout(xaxis=dict(range=[fra_dato, til_dato]),
+                                                            yaxis=dict(range=[0,120],title='Temperatur [°C]'),
+                                                            title="Ingen målinger i valgt periode!",
+                                                            #margin={'l':300,'r':100,'t':5,'b':50},
+                                                        )}
             else:
                 #tilordner X og Y på graf
                 X=ts_UTC
@@ -226,7 +237,7 @@ def callbacks(app):
     #Live målerelé graf
     @app.callback(Output('live-graph2', 'figure'),
                 [Input('graph-update2', 'n_intervals'),
-                Input('dropdown-område', 'label'),
+                Input('dropdown-område2', 'label'),
                 Input('måle-valg', 'value')],
                 [State(component_id='url', component_property='pathname'),
                 ]) 
@@ -256,18 +267,19 @@ def callbacks(app):
             til_dato = til_dato_UTC.astimezone(pytz.timezone('Europe/Oslo'))
             fra_dato= til_dato + relativedelta(hours=-offset)
             fra_dato=fra_dato.strftime("%Y-%m-%d %H:%M:%S")
-            print(dropdown_område)
-
             #Laster inn ny data fra database
             meterData = update_meterData(sløyfe_valg,fra_dato, til_dato)
             #Tilorder data til x og y etter målevalg
             X=meterData["timeReceived"]
             Y=meterData[målinger_dict[måle_valg]]
-            print(Y)
-
             #Hindrer at siden fryser
             if not X:
-                return {'data': [], 'layout': {}}
+                return {'data': [], 'layout' : go.Layout(xaxis=dict(range=[fra_dato,til_dato]),
+                                                            yaxis=dict(range=[0,100],
+                                                                        title=enhet_dict[måle_valg], tickangle=0,),
+                                                            title="Ingen målinger i valgt periode!",
+                                                            #margin={'l':100,'r':100,'t':50,'b':50},
+                                                            )}
             else:
                 #Returnerer graf med data og layout
                 data = plotly.graph_objs.Scatter(
@@ -286,8 +298,9 @@ def callbacks(app):
             with open('errors.txt','a') as f:
                 f.write(str(e))
                 f.write('\n')
+    #Oppdaterer knapp for måleområde til temperatur
     @app.callback(
-            Output(component_id='dropdown-område', component_property='label'),
+            Output(component_id='dropdown-område1', component_property='label'),
             [Input(component_id='1hours', component_property='n_clicks'),
             Input(component_id='6hours', component_property='n_clicks'),
             Input(component_id='12hours', component_property='n_clicks'),
@@ -296,6 +309,29 @@ def callbacks(app):
             ])
     def update_label(n_1hours, n_6hours, n_12hours, n_day, n_week):
         id_lookup = {'1hours':'Siste time', '6hours':'Siste 6 timer', '12hours':'Siste 12 timer', 'day':'Siste dag', 'week': 'Siste uke'}
+
+        ctx = dash.callback_context
+
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+        label_clicked = id_lookup[button_id]
+
+
+        if (n_1hours is None and n_6hours is None and n_12hours is None and n_day is None and n_week is None) or not ctx.triggered:
+            return 'Siste 6 timer'
+
+        return label_clicked
+    
+    #Oppdaterer knapp for måleområde til rele
+    @app.callback(
+            Output(component_id='dropdown-område2', component_property='label'),
+            [Input(component_id='1hours_rele', component_property='n_clicks'),
+            Input(component_id='6hours_rele', component_property='n_clicks'),
+            Input(component_id='12hours_rele', component_property='n_clicks'),
+            Input(component_id='day_rele', component_property='n_clicks'),
+            Input(component_id='week_rele', component_property='n_clicks'),
+            ])
+    def update_label(n_1hours, n_6hours, n_12hours, n_day, n_week):
+        id_lookup = {'1hours_rele':'Siste time', '6hours_rele':'Siste 6 timer', '12hours_rele':'Siste 12 timer', 'day_rele':'Siste dag', 'week_rele': 'Siste uke'}
 
         ctx = dash.callback_context
 
